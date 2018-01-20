a7f6b29f95859323357e53c8f38ea46459e81dfc
FeMaidens <FeMaidens@FeMaidens-PC.BXSCI.78X445.nycboe.org>
2018-01-20 14:10:17 -0500

    PID

--- src/org/usfirst/frc/team2265/robot/subsystems/PIDDrive.java
+++ src/org/usfirst/frc/team2265/robot/subsystems/PIDDrive.java
@@ -7,7 +7,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
-public class PIDDrive extends PIDSubsystem {//You're doing great Zarrin! Keep it up! I like the PID drive class =)
+public class PIDDrive extends PIDSubsystem {  //You're doing great Zarrin! Keep it up! I like the PID drive class =)
 	public static TalonSRX frontLeft = new TalonSRX(RobotMap.frontLeftPort);
 	public static TalonSRX frontRight = new TalonSRX(RobotMap.frontRightPort);
 	public static TalonSRX rearLeft = new TalonSRX(RobotMap.rearLeftPort);
@@ -15,8 +15,13 @@ public class PIDDrive extends PIDSubsystem {//You're doing great Zarrin! Keep it
 	
 	public static AnalogGyro gyro = new AnalogGyro(RobotMap.gyroPort);
 
-	public PIDDrive(String name, double p, double i, double d) {
-		super("PIDDrive", 0.1, 0, 0.1);		// TODO Auto-generated constructor stub
+	public PIDDrive(String name, double kp, double ki, double kd) {
+		super("PIDDrive", 0.1, 0, 0.1);	
+		gyro.reset();
+		getPIDController().setAbsoluteTolerance(20.0);
+		getPIDController().setContinuous(true);
+		
+		// TODO Auto-generated constructor stub
 	}
 
 	
