d30a71d2f10fc65c245f25d16a13781c1de68aa8
FeMaidens <FeMaidens@FeMaidens-PC.mynycschools.org>
2018-01-24 15:09:05 -0500

    edited PIDDrive

--- src/org/usfirst/frc/team2265/robot/subsystems/PIDDrive.java
+++ src/org/usfirst/frc/team2265/robot/subsystems/PIDDrive.java
@@ -1,29 +1,46 @@
 package org.usfirst.frc.team2265.robot.subsystems;
 
+import org.usfirst.frc.team2265.robot.Robot;
 import org.usfirst.frc.team2265.robot.RobotMap;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-import edu.wpi.first.wpilibj.AnalogGyro;
+import org.usfirst.frc.team2265.robot.subsystems.Drivetrain;
+//import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 public class PIDDrive extends PIDSubsystem {  //You're doing great Zarrin! Keep it up! I like the PID drive class =)
-	public static TalonSRX frontLeft = new TalonSRX(RobotMap.frontLeftPort);
+	/*public static TalonSRX frontLeft = new TalonSRX(RobotMap.frontLeftPort);
 	public static TalonSRX frontRight = new TalonSRX(RobotMap.frontRightPort);
 	public static TalonSRX rearLeft = new TalonSRX(RobotMap.rearLeftPort);
-	public static TalonSRX rearRight = new TalonSRX(RobotMap.rearRightPort);
+	public static TalonSRX rearRight = new TalonSRX(RobotMap.rearRightPort);*/
 	
-	public static AnalogGyro gyro = new AnalogGyro(RobotMap.gyroPort);
-
+	//public static AnalogGyro gyro = new AnalogGyro(RobotMap.gyroPort);
+   
 	public PIDDrive(String name, double kp, double ki, double kd) {
 		super("PIDDrive", 0.1, 0, 0.1);	
-		gyro.reset();
+		
+		//sensorMode = SensorMode.ENCODER;
+		//driveMode = DriveMode.GYRO;
+		
+		Drivetrain.encoderLeft.reset();
+		Drivetrain.encoderRight.reset();
+		
 		getPIDController().setAbsoluteTolerance(20.0);
 		getPIDController().setContinuous(true);
 		
+		
+				
 		// TODO Auto-generated constructor stub
 	}
+	
+	public enum SensorMode {
+		GYRO, ENCODER;
+	}
 
+	public enum DriveMode {
+		TURN, DRIVE;
+	}
+	
 	
 	@Override
 	protected double returnPIDInput() {
