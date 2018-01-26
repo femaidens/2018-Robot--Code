30b881923efc5e66a4e9a27e3f72c287ee98a85b
carolshao <caroshao630@gmail.com>
2018-01-26 13:42:55 -0500

    updated PID commands

---
+++ src/org/usfirst/frc/team2265/robot/commands/AdjustWithSensor.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team2265.robot.commands;
+
+import org.usfirst.frc.team2265.robot.Robot;
+import org.usfirst.frc.team2265.robot.subsystems.PIDDrive.SensorMode;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class AdjustWithSensor extends Command{
+
+	public AdjustWithSensor() {
+		requires(Robot.drive);
+		// Makes command interruptible
+		setInterruptible(true);
+		
+	}
+	@Override
+	protected void initialize() {
+		// Calibrates the turn angle
+		if(Robot.drive.sensorMode == SensorMode.ENCODER) {
+			Robot.drive.startAdjust(Robot.drive.getREncDistance(), SmartDashboard.getNumber("Right Drive", 0));
+			Robot.drive.startAdjust(Robot.drive.getLEncDistance(), SmartDashboard.getNumber("Left Drive", 0));
+		}
+
+		else
+			Robot.drive.startAdjust(Robot.drive.gyro.getAngle(), SmartDashboard.getNumber("Turn", 0));
+		    // Sets PID of the PID controller to the values given on the SmartDashboard
+			Robot.drive.getPIDController().setPID(SmartDashboard.getNumber("P", 0.1), SmartDashboard.getNumber("I", 0), SmartDashboard.getNumber("D", 0.1));
+	}
+
+	@Override
+	protected void execute() {
+		// Inserts the current angle of the gyro onto the SmartDashboard
+		SmartDashboard.putNumber("GyroValue", Robot.drive.gyro.getAngle());
+		SmartDashboard.putNumber("EncoderValue Right", Robot.drive.getREncDistance());
+		SmartDashboard.putNumber(("EncoderValue Right"), Robot.drive.getLEncDistance());
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// Ends when the robot is at the desired angle
+		return Robot.drive.getPIDController().onTarget();
+	}
+
+	@Override
+	protected void end() {
+		Robot.drive.endAdjust();	
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
+}
---
+++ src/org/usfirst/frc/team2265/robot/commands/PIDDriveCommand.java
@@ -0,0 +1,63 @@
+package org.usfirst.frc.team2265.robot.commands;
+
+import org.usfirst.frc.team2265.robot.Robot;
+import org.usfirst.frc.team2265.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team2265.robot.subsystems.PIDDrive.DriveMode;
+import org.usfirst.frc.team2265.robot.subsystems.PIDDrive.SensorMode;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+/**
+ *
+ */
+public class PIDDriveCommand extends Command {
+	
+	private Joystick driveJoystick;
+    public PIDDriveCommand(Joystick stick) {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.drive);
+        // Initialize joystick used for setting speed.
+       driveJoystick = stick;
+        // Makes command interruptible.
+        setInterruptible(true);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	if(SmartDashboard.getString("Turning?", "true").toLowerCase().equals("true")) {
+    		Robot.drive.sensorMode = SensorMode.ENCODER;
+    		Robot.drive.driveMode = DriveMode.TURN;
+    	}
+    	else {
+    		Robot.drive.sensorMode = SensorMode.GYRO;
+    		Robot.drive.driveMode = DriveMode.DRIVE;
+    	}
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    		// Puts the current value of the gyro onto the Smart Dashboard
+    		SmartDashboard.putNumber("EncoderL Value", Drivetrain.encoderLeft.getDistance());
+    		SmartDashboard.putNumber("EncoderR Value", Drivetrain.encoderRight.getDistance());
+    		// Rotates the robot; speed correlated with magnitude of joystick on the y axis
+    		Robot.drive.setSpeed(-driveJoystick.getY());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	    Robot.drive.setSpeed(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    		end();
+    }
+    
+}
